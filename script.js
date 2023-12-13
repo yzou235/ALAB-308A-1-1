87e6c621fc07c3eb858292a6a17d66fadd252977
yzou235 <yumingzou235@gmail.com>
2023-12-13 16:04:08 -0500

    Finished part 2

--- script.js
+++ script.js
@@ -13,4 +13,32 @@ try {
     console.log(counter);
 }
 
-//
+// Part 2: Trampolines
+
+// Write a recursive function that completely flattens an array of nested arrays, regardless of how deeply nested the arrays are.
+// Once your recursive function is complete, trampoline it.
+
+function flattenArray(arr){
+    let result = arr.reduce((acc, cur) => {
+        if (Array.isArray(cur)) {
+            acc = acc.concat(flattenArray(cur));
+        } else {
+            acc.push(cur);
+        }
+        return acc
+    }, []);
+    return result;
+}
+
+let array = [1, [2, 3], [4, [5, 6]]];
+console.log(flattenArray(array));
+
+// Part 3: Deferred Execution
+
+// Create a simple HTML element to hold text. Cache this HTML element into a JavaScript variable.
+
+
+// Write a function that takes a parameter n and adds a list of all prime numbers between one and n to your HTML element.
+
+
+// Once complete, use the alert() method to alert the user that the calculation is finished.
